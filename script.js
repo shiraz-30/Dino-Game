d82e25c263f68147aea92b1375f44e97afdadcc1
Shiraz Mangat <78081835+shiraz-30@users.noreply.github.com>
2021-10-06 11:32:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const dino = document.getElementById("dino");
+const rock = document.getElementById("rock");
+const score = document.getElementById("score");
+
+function jump() {
+
+  dino.classList.add("jump-animation");
+
+  setTimeout(() =>
+    dino.classList.remove("jump-animation"), 500);
+
+}
+
+document.addEventListener('keypress', (event) => {
+
+  if (!dino.classList.contains('jump-animation')) {
+    jump();
+
+  }
+
+})
+
+setInterval(() => {
+
+  const dinoTop = parseInt(window.getComputedStyle(dino)
+    .getPropertyValue('top'));
+
+  const rockLeft = parseInt(window.getComputedStyle(rock)
+    .getPropertyValue('left'));
+
+  score.innerText++;
+
+  if (rockLeft < 0) {
+
+    rock.style.display = 'none';
+
+  } else {
+
+    rock.style.display = ''
+
+  }
+
+  if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
+
+    alert("You got a score of: " + (score.innerText - 1) +
+      "\n\nPlay again?");
+
+    location.reload();
+  }
+
+}, 50);
+
